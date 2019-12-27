24d808523347d4b30f8f0088b6e697b1e1016368
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-27 19:00:32 +0000

    Application class

---
+++ app/src/main/java/com/titan/flickrapp/App.java
@@ -0,0 +1,16 @@
+package com.titan.flickrapp;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Timber.plant(new Timber.DebugTree());
+    }
+
+}
