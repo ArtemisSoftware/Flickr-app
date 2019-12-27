4c22f582e99102c049f2c4775610f22c71aa0d4a
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-27 18:52:49 +0000

    Api constants

---
+++ app/src/main/java/com/titan/flickrapp/util/ApiConstants.java
@@ -0,0 +1,12 @@
+package com.titan.flickrapp.util;
+
+public class ApiConstants {
+
+    public static final String BASE_URL = "https://api.flickr.com/services/";
+
+    public static final String API_KEY = "22df34dbfb05acc69192986065aec6c0";
+
+
+    public static final String FORMAT = "json";
+    public static final String NO_JSON_CALLBACK = "?";
+}
