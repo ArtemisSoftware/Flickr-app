b934e8e57f904467d015fe28502664028fc402eb
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-27 22:58:19 +0000

    Base Activity

---
+++ app/src/main/java/com/titan/flickrapp/ui/BaseActivity.java
@@ -0,0 +1,32 @@
+package com.titan.flickrapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.titan.flickrapp.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    private ProgressBar progress_bar;
+
+    @Override
+    public void setContentView(int layoutResID) {
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
+        FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
+        progress_bar = constraintLayout.findViewById(R.id.progress_bar);
+
+        getLayoutInflater().inflate(layoutResID, frameLayout, true);
+        super.setContentView(constraintLayout);
+    }
+
+    public void showProgressBar(boolean visible) {
+
+        progress_bar.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+    }
+}
