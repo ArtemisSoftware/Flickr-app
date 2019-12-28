9c1b88d34727ed8af2fc77e8778cd8d15141b158
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-28 15:02:08 +0000

    Photo api methods

--- app/src/main/java/com/titan/flickrapp/requests/FlickrApi.java
+++ app/src/main/java/com/titan/flickrapp/requests/FlickrApi.java
@@ -1,5 +1,7 @@
 package com.titan.flickrapp.requests;
 
+import com.titan.flickrapp.requests.responses.PhotoListResponse;
+import com.titan.flickrapp.requests.responses.PhotoResponse;
 import com.titan.flickrapp.requests.responses.UserSearchResponse;
 
 import io.reactivex.Observable;
@@ -11,5 +13,11 @@ public interface FlickrApi {
 
     @GET("rest/?")
     Observable<UserSearchResponse> searchUser(@Query("method") String method, @Query("username") String username);
+
+    @GET("rest/?")
+    Observable<PhotoListResponse> searchPhotoList(@Query("method") String method, @Query("user_id") String nsid, @Query("page") String page);
+
+    @GET("rest/?")
+    Observable<PhotoResponse> searchPhoto(@Query("method") String method, @Query("user_id") String nsid, @Query("page") String page);
 
 }
